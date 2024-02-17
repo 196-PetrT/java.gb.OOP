08279e498c11483658166b04061925d319c35d09
PetrT <140140070+196-PetrT@users.noreply.github.com>
2024-02-17 13:30:26 +0300

    add lec 6

--- src/Lesson_11/Ex001Calc/Model.java
+++ src/Lesson_11/Ex001Calc/Model.java
@@ -1,9 +1,10 @@
 package Lesson_11.Ex001Calc;
 
 public interface Model {
-    int result();
 
     void setX(int value);
 
     void setY(int value);
+
+    int result();
 }
--- src/Lesson_11/Ex002Phonebook/Config.java
+++ src/Lesson_11/Ex002Phonebook/Config.java
@@ -1,5 +1,5 @@
 package Lesson_11.Ex002Phonebook;
 
 public class Config {
-    public static String pathDb = "data.db";
+    public static String pathDb = "D:\\обучение в GB\\repForJava\\java.gb.OOP\\src\\Lesson_11\\data.db";
 }
